2a715924a170232af7a1dfa7b86a0cf419325482
Soumyajit <xeltondemon999@gmail.com>
2026-05-01 04:04:41 +0530

    Added Profile

--- src/app/nav/profile/profile.ts
+++ src/app/nav/profile/profile.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profile',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {}
+export class Profile {
+  // Mock user data based on your demographics
+  userName = signal('Soumyajit Maity');
+  userRole = signal('Admin / Student');
+  userEmail = signal('admin@hrms.com');
+  userPhone = signal('+91 9876543210');
+  userLocation = signal('Kolkata, West Bengal');
+}
