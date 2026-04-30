b590d5f2dacd90b2f9d12535099f64db954d48d8
Soumyajit <xeltondemon999@gmail.com>
2026-05-01 04:00:49 +0530

    Added Payroll

--- src/app/payroll/payroll.ts
+++ src/app/payroll/payroll.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,inject,signal,computed } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { FilterDialog } from '../filter-dialog/filter-dialog';
 
 @Component({
   selector: 'app-payroll',
@@ -6,4 +8,45 @@ import { Component } from '@angular/core';
   templateUrl: './payroll.html',
   styleUrl: './payroll.css',
 })
-export class Payroll {}
+export class Payroll {
+    private readonly dialog = inject(MatDialog);
+    // Initialize with the actual current date
+  viewDate = signal(new Date());
+
+  // Dynamically format the month and year
+  monthDisplay = computed(() => {
+    return this.viewDate().toLocaleString('default', { month: 'long', year: 'numeric' });
+  });
+
+  openFilter() {
+    const dialogRef = this.dialog.open(FilterDialog, {
+      width: '400px',
+      panelClass: 'custom-filter-panel' // For custom Tailwind styling
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Filters applied:', result);
+        // Refresh your table data here
+      }
+    });
+  }
+
+  // Logic to move between payroll periods
+  nextMonth() {
+    this.viewDate.update(date => {
+      const next = new Date(date);
+      next.setMonth(next.getMonth() + 1);
+      return next;
+    });
+  }
+
+  prevMonth() {
+    this.viewDate.update(date => {
+      const prev = new Date(date);
+      prev.setMonth(prev.getMonth() - 1);
+      return prev;
+    });
+  }
+
+}
